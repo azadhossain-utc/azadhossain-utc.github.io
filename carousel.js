828f4e629c636f3a4e8941233d8bb4927a32d16d
Tamjeed Azad <26588289+tamjazad@users.noreply.github.com>
2020-01-14 01:35:09 -0500

    added image carousel

---
+++ carousel.js
@@ -0,0 +1,31 @@
+/*
+from original repo: https://github.com/tamjazad/image-carousel
+MIT LICENSE
+*/
+
+const number_of_imgs = 37;
+const image_display_time = 3000;
+
+function updateImage(index) {
+	var str = "carousel_images/img" + index + ".jpg";
+	document.getElementById("carousel").setAttribute("src", str);
+	console.log(str);
+}
+
+function imageCarousel() {
+	updateImage(1);
+	console.log(1);
+	let i = 2;
+	let j = 1;
+	while (j < number_of_imgs) {
+		setTimeout(function() {
+			updateImage(i);
+			console.log(i);
+			i++;
+		}, j * image_display_time);
+		j++;
+	}
+}
+
+imageCarousel();
+var carousel = setInterval(imageCarousel, (number_of_imgs) * image_display_time);
